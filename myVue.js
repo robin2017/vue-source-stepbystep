0bf62f168b735b57314779cfae59bc66df4f4945
robin2017 <robin.seu@foxmail.com>
2018-11-30 21:03:34 +0800

    step2-双向绑定框架模型：配置和模版交给用户，绑定过程交给框架

---
+++ myVue.js
@@ -0,0 +1,20 @@
+/**
+ * 解析dom
+ * */
+function compile(dom, vm) {
+
+}
+
+/**
+ * 将dom拦截到fragment
+ * */
+function nodeToFragment(dom, vm) {
+
+}
+
+/**
+ * 构造函数
+ * */
+function Vue(options) {
+
+}
