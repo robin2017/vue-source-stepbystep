7f5c73253b34f579c5f4a9284024d1f7e9bf52e1
robin2017 <robin.seu@foxmail.com>
2018-12-01 16:30:36 +0800

    step8:双向绑定的优化

--- myVue.js
+++ myVue.js
@@ -2,13 +2,12 @@
  * 事件对象 ==> 对应data的一个属性
  * 两个方法：notify/addSub
  * */
-//全局变量：dep映射
-deps = {};
-
 function Dep() {
     this.subs = [];
 }
 
+//全局变量放在构造器上
+Dep.deps = {};
 Dep.prototype = {
     notify: function () {
         this.subs.forEach(sub => {
@@ -24,10 +23,11 @@ Dep.prototype = {
  * 订阅者 ==>对应data某个属性的一个绑定节点
  * 两个方法:update/register
  * */
-function Watcher(vm, name, dom) {
+function Watcher(vm, name, dom, dep) {
     this.vm = vm;
     this.name = name;
     this.dom = dom;
+    this.register(dep);
 }
 
 Watcher.prototype = {
@@ -40,13 +40,9 @@ Watcher.prototype = {
     }
 };
 
-
 /**
  * 编译dom
  * */
-//全局变量：正则表达式
-regEx = /\{\{(.*)\}\}/;
-
 function compile(dom, vm) {
     if (dom.nodeType === 1) {//元素节点
         let attrs = dom.attributes;
@@ -59,20 +55,21 @@ function compile(dom, vm) {
                 dom.addEventListener('input', function (evt) {
                     vm[name] = evt.target.value;
                 });
-                let watcher = new Watcher(vm, name, dom);
-                watcher.register(deps[name])
+                new Watcher(vm, name, dom, Dep.deps[name]);
             }
         }
     } else if (dom.nodeType === 3) {//文本节点
-        if (regEx.test(dom.nodeValue)) {
+        if (compile.regEx.test(dom.nodeValue)) {
             let name = RegExp.$1.trim();
             //初始化data-->view （文本节点）
             dom.nodeValue = vm.data[name];
-            let watcher = new Watcher(vm, name, dom);
-            watcher.register(deps[name])
+            new Watcher(vm, name, dom, Dep.deps[name]);
         }
     }
 }
+
+//全局变量放在构造器上
+compile.regEx = /\{\{(.*)\}\}/;
 
 /**
  * dom劫持到fragment
@@ -87,7 +84,6 @@ function nodeToFragment(dom, vm) {
         }
         compile(child, vm);
         fragment.appendChild(child);
-
     }
     return fragment;
 }
@@ -97,7 +93,7 @@ function nodeToFragment(dom, vm) {
  * */
 function proxyData(vm) {
     Object.keys(vm.data).forEach(key => {
-        deps[key] = new Dep();
+        Dep.deps[key] = new Dep();
         Object.defineProperty(vm, key, {
             get: function () {
                 return vm.data[key];
@@ -105,7 +101,7 @@ function proxyData(vm) {
             set: function (newValue) {
                 vm.data[key] = newValue;
                 //发布者发布消息，则事件对象通知
-                deps[key].notify();
+                Dep.deps[key].notify();
             }
         })
     })
